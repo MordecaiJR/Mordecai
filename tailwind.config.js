d50756c177f470b89d98368db8fa337e8895e0b9
Mordecai <jr.assis80@gmail.com>
2024-12-26 18:46:18 -0300

    etapa da main

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,22 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'eclipse-background' : 'url("/src/assets/imagens/Ellipse_2.png"), radial-gradient(50% 50% at 50.01% 50.01%, #FFFFFF 0%, #E7EFFA 100%)',
+        'bg-circle' : 'url("src/assets/imagens/bg-circle.svg")'
+      },
+    
+      colors: {
+        customBlack : '#02033B',
+        customWhite : '#F3F8FF',
+        CustomText : '#02033B',
+        customBg: '#FFC247',
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+    },
   },
   plugins: [],
 }
-
+}
